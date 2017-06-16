19e7fe334fb1d3faba4a8f549e6ec5cd8bb07430
wk <964269669@qq.com>
2017-06-16 16:11:04 +0800

    引导页功能

---
+++ www/areas/tab/tab_route.js
@@ -0,0 +1,14 @@
+
+angular.module('tab.route', ['tab.controller'])
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'areas/tab/tab.html',
+        controller: 'TabCtrl'
+      })
+
+  });
---
+++ www/js/global.js
@@ -0,0 +1,7 @@
+// 全局变量模块
+angular.module('global', [])
+		.constant("GlobalVariable",{
+				'HTTP':"https",
+				'SERVER_PATH':'http://192.168.1.1:8080/',// 服务器地址
+				'VERSION':"1.0.1"
+		})
